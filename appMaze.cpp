5534b9070edb04940115b7dc9c723bfcee553b75
wfdudley <37313619+wfdudley@users.noreply.github.com>
2020-09-28 15:20:24 -0400

    Add files via upload

--- appMaze.cpp
+++ appMaze.cpp
@@ -39,7 +39,7 @@ bool unvisitedCells(vector <Room> mazeGrid) {
 int checkNeighbours(vector <Room> maze, Room r) {
   int x = r.getX();
   int y = r.getY();
-  vector < Room > neighbours;
+  vector <Room> neighbours;
   if(!maze[(x - 1) * ROWS + y].isVisited() && x > 0) {
     neighbours.push_back(maze[(x - 1) * ROWS + y]);
   }
@@ -61,10 +61,32 @@ int checkNeighbours(vector <Room> maze, Room r) {
   return nxt;
 }
 
+int hasNeighbor(vector <Room> maze, Room r, int direction) {
+  int x = r.getX();
+  int y = r.getY();
+  // walls are: 0,1,2,3 -> up, right, down, left
+  switch(direction) {
+    case 0 :	// up
+      if(y > 0) return x * ROWS + y - 1;
+      break;
+    case 1 :`	// right
+      if(x < ROWS - 1) return (x + 1) * ROWS + y;
+      break;
+    case 2 :	// down
+      if(y < COLUMNS - 1) return x * ROWS + y + 1;
+      break;
+    case 3 :	// left
+      if(x > 0) return (x - 1) * ROWS + y;
+      break;
+  }
+  return -1;
+}
+
 void appMaze (void) {
 int16_t x, y;
 uint16_t dcolor;
 uint32_t chkn_count = 0;
+int mSelect;
   maze_built = false;
   Serial.println(F("appMaze()"));
   tft->fillScreen(TFT_BLACK); // Clear screen
@@ -86,7 +108,7 @@ uint32_t chkn_count = 0;
   }
 
   Room *current = &mazeGrid[0]; 
-  current->visit(); 
+  current->visit(true); 
 
   while (!maze_built && running) {
     my_idle();
@@ -112,7 +134,7 @@ uint32_t chkn_count = 0;
       current->removeWalls(nextRoom);
       // Make the chosen cell the current cell and mark it as visited
       current = &nextRoom; 
-      current->visit();
+      current->visit(true);
     } else if (!roomStack.empty()) { // If stack is not empty
       // Pop a cell from the stack
       Room &previousRoom = *roomStack.top();
@@ -137,21 +159,72 @@ uint32_t chkn_count = 0;
       mazeGrid[i].show();
     }
     
-    dcolor = TFT_DARKGREY;
     int xCoordHead = current->getX() * ROOM_WIDTH;
     int yCoordHead = current->getY() * ROOM_WIDTH;
     // Serial.printf("l:%d %d %d %d %d %d\n", __LINE__, xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, dcolor);
     // this "cursor" shows the room we're "visiting":
-    tft->fillRect(xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, dcolor);
+    tft->fillRect(xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_DARKGREY);
 
   }
   bool touched = false;
   if(maze_built) {	// then "play" the game (as yet, unwritten)
+  int xCoordHead = current->getX() * ROOM_WIDTH;
+  int yCoordHead = current->getY() * ROOM_WIDTH;
+  Serial.printf("l:%d %d %d %d %d %d\n", __LINE__, xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_WHITE);
+  tft->fillRect(xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_WHITE);
+  current = &mazeGrid[rand() % ROWS]; 
+  int xCoordHead = current->getX() * ROOM_WIDTH;
+  int yCoordHead = current->getY() * ROOM_WIDTH;
+  Serial.printf("l:%d %d %d %d %d %d\n", __LINE__, xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_BLUE);
+  tft->fillRect(xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_BLUE);
     do {
       my_idle();
-      if(ttgo->getTouch(x, y)) { // if touch
+      // walls are: 0,1,2,3 -> up, right, down, left
+      mSelect = poll_swipe_or_menu_press();	// poll for touch, returns 0-15
+      int next = -1;
+      if(mSelect == (UP + 11)) { // swipe 12 = up, 13 = down, left = 14, rt = 15
+	// if up wall missing, move up
+	if(!current->hasWall(0)) {
+	  next = hasNeighbour(mazeGrid, *current, 0);
+	}
+      }
+      else if(mSelect == (DOWN + 11)) {
+	// if down wall missing, move down
+	if(!current->hasWall(2)) {
+	  next = hasNeighbour(mazeGrid, *current, 2);
+	}
+      }
+      else if(mSelect == (LEFT + 11)) {
+	// if left wall missing, move left
+	if(!current->hasWall(3)) {
+	  next = hasNeighbour(mazeGrid, *current, 3);
+	}
+      }
+      else if(mSelect == (RIGHT + 11)) {
+	// if right wall missing, move right
+	if(!current->hasWall(1)) {
+	  next = hasNeighbour(mazeGrid, *current, 1);
+	}
+      }
+      else if(mSelect == (CWCIRCLE + 11)) {
 	touched = true;
 	break;
+      }
+      if(next > -1) {
+	int xCoordHead = current->getX() * ROOM_WIDTH;
+	int yCoordHead = current->getY() * ROOM_WIDTH;
+	Serial.printf("l:%d %d %d %d %d %d\n", __LINE__, xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_WHITE);
+	tft->fillRect(xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_WHITE);
+	Room &nextRoom = mazeGrid[next];
+	current = &nextRoom; 
+	int xCoordHead = current->getX() * ROOM_WIDTH;
+	int yCoordHead = current->getY() * ROOM_WIDTH;
+	Serial.printf("l:%d %d %d %d %d %d\n", __LINE__, xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_BLUE);
+	// this "cursor" shows the room we're "visiting":
+	tft->fillRect(xCoordHead, yCoordHead, ROOM_WIDTH, ROOM_WIDTH, TFT_BLUE);
+      }
+      else {
+	beep(3); beep(0);
       }
     } while(!touched);
   }
