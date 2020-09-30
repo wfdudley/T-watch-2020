7eb580675b70e60f09a53f273d08e615e0720d91
wfdudley <37313619+wfdudley@users.noreply.github.com>
2020-09-30 15:28:33 -0400

    removed old code after move to AceTime library.

--- Analog_Time.cpp
+++ Analog_Time.cpp
@@ -239,49 +239,7 @@ void update_clock_face(void) {
 void Analog_Time(uint8_t fullUpdate) {
   // Get the current time and date
   get_time_in_tz(tzindex);
-#if 0
-  tnow = ttgo->rtc->getDateTime();
-#define TIME_IS_GMT 1
-#if TIME_IS_GMT
-struct tm timeinfo;
-time_t utc_time, unix_time;
-  timeinfo.tm_hour = tnow.hour;
-  timeinfo.tm_min  = tnow.minute;
-  timeinfo.tm_sec  = tnow.second;
-  timeinfo.tm_mday = tnow.day;
-  timeinfo.tm_mon  = tnow.month - 1;
-  timeinfo.tm_year = tnow.year - 1900;
-#if 0
-  if(!(tnow.second % 5)) {
-    Serial.print(F("UTC time: "));
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  }
-#endif
-  utc_time = mktime(&timeinfo);
-  unix_time = timelocal(utc_time);
-  memcpy(&timeinfo, localtime(&unix_time), sizeof(struct tm)/sizeof(char));
-#if 0
-  if(!(tnow.second % 5)) {
-    Serial.printf("tzindex = %d, local time: ", tzindex);
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  }
-#endif
-  hh = timeinfo.tm_hour;
-  mm = timeinfo.tm_min;
-  ss = timeinfo.tm_sec;
-  // wday = timeinfo.tm_wday;
-  dday = timeinfo.tm_mday;
-  mmonth = 1 + timeinfo.tm_mon;
-  yyear = 1900 + timeinfo.tm_year;
-#else
-  hh = tnow.hour;
-  mm = tnow.minute;
-  ss = tnow.second;
-  dday = tnow.day;
-  mmonth = tnow.month;
-  yyear = tnow.year;
-#endif
-#endif
+
   local_hour = hh;
   local_minute = mm;
   tft->setTextSize(1);
