3be7c5c7c6c35447774298757e186116351b9876
William F. Dudley Jr <37313619+wfdudley@users.noreply.github.com>
2020-12-01 18:14:36 -0500

    moved beep files to directory "sounds"

--- mp3files.h
+++ mp3files.h
@@ -1,15 +1,15 @@
 // mp3's of beeps of various frequencies
 // W.F.Dudley Jr.
 
-#include "beep__182.h"	// 182 Hz 100 msec
-#include "beep__220.h"	// 220 Hz 100 msec
-#include "beep__279.h"	// etc.
-#include "beep__303.h"
-#include "beep__473.h"
-#include "beep__576.h"
-#include "beep_1050.h"	// 1050 Hz 100 msec
-#include "beep_1050L.h"	// 1050 Hz 200 msec
-#include "beep-chirp1.h"
+#include "sounds/beep__182.h"	// 182 Hz 100 msec
+#include "sounds/beep__220.h"	// 220 Hz 100 msec
+#include "sounds/beep__279.h"	// etc.
+#include "sounds/beep__303.h"
+#include "sounds/beep__473.h"
+#include "sounds/beep__576.h"
+#include "sounds/beep_1050.h"	// 1050 Hz 100 msec
+#include "sounds/beep_1050L.h"	// 1050 Hz 200 msec
+#include "sounds/beep-chirp1.h"
 
 const unsigned char* const beep_array[] PROGMEM = {
     beep__182_mp3,
