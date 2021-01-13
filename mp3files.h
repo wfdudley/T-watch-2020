626ce101b414102e99425ca03fe219e2ef56a462
William F. Dudley Jr <37313619+wfdudley@users.noreply.github.com>
2021-01-13 14:31:27 -0500

    changes for StopWatch beep function.

--- mp3files.h
+++ mp3files.h
@@ -10,6 +10,11 @@
 #include "sounds/beep_1050.h"	// 1050 Hz 100 msec
 #include "sounds/beep_1050L.h"	// 1050 Hz 200 msec
 #include "sounds/beep-chirp1.h"
+#include "sounds/C5_L.h"
+#include "sounds/E5_L.h"
+#include "sounds/G5_L.h"
+#include "sounds/SWEEP.h"
+#include "sounds/TRIPLE.h"
 
 const unsigned char* const beep_array[] PROGMEM = {
     beep__182_mp3,
@@ -20,7 +25,12 @@ const unsigned char* const beep_array[] PROGMEM = {
     beep__576_mp3,
     beep_1050_mp3,
     beep_1050L_mp3,
-    beep_chirp1_mp3
+    beep_chirp1_mp3,
+    C5_L_mp3,
+    E5_L_mp3,
+    G5_L_mp3,
+    SWEEP_mp3,
+    TRIPLE_mp3
 };
 
 const uint16_t beep_sizes[] = {
@@ -32,5 +42,10 @@ const uint16_t beep_sizes[] = {
     sizeof(beep__576_mp3),
     sizeof(beep_1050_mp3),
     sizeof(beep_1050L_mp3),
-    sizeof(beep_chirp1_mp3)
+    sizeof(beep_chirp1_mp3),
+    sizeof(C5_L_mp3),
+    sizeof(E5_L_mp3),
+    sizeof(G5_L_mp3),
+    sizeof(SWEEP_mp3),
+    sizeof(TRIPLE_mp3)
 };
